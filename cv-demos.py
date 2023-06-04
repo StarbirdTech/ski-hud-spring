cc6b7e06c27e822f381652e3d39c51c942a78948
StarbirdTech <64431622+StarbirdTech@users.noreply.github.com>
2023-06-04 14:31:10 -0400

    add timer and other info

--- cv-demos.py
+++ cv-demos.py
@@ -1,15 +1,27 @@
 import cv2
+import time
 
-video_path = "src/SkiHUD-demo-video.mp4"
+
+def estimate_remaining_time(current_frame, total_frames, start_time):
+    elapsed_time = time.time() - start_time
+    frames_remaining = total_frames - current_frame
+    seconds_per_frame = elapsed_time / current_frame
+    remaining_time = frames_remaining * seconds_per_frame
+    return remaining_time
+
+
+video_path = "cache/NoOverlay.mp4"
 cap = cv2.VideoCapture(video_path)
 fps = cap.get(cv2.CAP_PROP_FPS)
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 out = cv2.VideoWriter(
-    "output.mp4",
-    cv2.VideoWriter_fourcc(*"H264"),
+    "cache/SkiHUD-winter1-cv.mp4",
+    cv2.VideoWriter_fourcc(*"avc1"),
     fps,
-    (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+    (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))),
 )
+start_time = time.time()
+print(f"Total frames: {frame_count}")
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -21,6 +33,13 @@ while cap.isOpened():
     kp, des = orb.detectAndCompute(gray_image, None)
     kp_image = cv2.drawKeypoints(frame, kp, None, color=(0, 255, 0), flags=0)
     out.write(kp_image)
+
+    current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+    remaining_time = estimate_remaining_time(current_frame, frame_count, start_time)
+    print(
+        f"Frame {current_frame+1}/{frame_count} | Remaining time: {remaining_time:.2f} seconds",
+        end="\r",
+    )
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
